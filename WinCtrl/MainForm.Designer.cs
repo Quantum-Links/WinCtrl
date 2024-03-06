02afe5643204e66cf8a79f51229339788480df3e
姚灵 <Quantum_Links@outlook.com>
2024-03-06 15:30:23 +0800

    添加项目文件。

---
+++ WinCtrl/MainForm.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace WinCtrl
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+			this.components = new System.ComponentModel.Container();
+			this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+			this.contextMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
+			this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+			this.contextMenu.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// notifyIcon
+			// 
+			this.notifyIcon.ContextMenuStrip = this.contextMenu;
+			this.notifyIcon.Tag = "控制管理";
+			this.notifyIcon.Text = "PC控制";
+			this.notifyIcon.Visible = true;
+			// 
+			// contextMenu
+			// 
+			this.contextMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1});
+			this.contextMenu.Name = "contextMenu";
+			this.contextMenu.Size = new System.Drawing.Size(101, 26);
+			// 
+			// toolStripMenuItem1
+			// 
+			this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+			this.toolStripMenuItem1.Size = new System.Drawing.Size(100, 22);
+			this.toolStripMenuItem1.Text = "关闭";
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(304, 201);
+			this.ControlBox = false;
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.Name = "MainForm";
+			this.ShowIcon = false;
+			this.ShowInTaskbar = false;
+			this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+			this.Text = "MainForm";
+			this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+			this.contextMenu.ResumeLayout(false);
+			this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+        private System.Windows.Forms.ContextMenuStrip contextMenu;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+    }
+}
+
---
+++ WinCtrl/MainForm.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+
+using Microsoft.Win32;
+
+
+namespace WinCtrl
+{
+    public partial class MainForm : Form
+    {
+        byte[] success = Encoding.UTF8.GetBytes("success");
+        public MainForm()
+        {
+            InitializeComponent();
+            contextMenu.ItemClicked += ContextMenu_ItemClicked;
+            UdpClient udpClient = new UdpClient(10001);
+            AsyncReceive(udpClient);
+            SetStartup(true);
+        }
+        async void AsyncReceive(UdpClient udpClient)
+        {
+            while (true)
+            {
+                try
+                {
+                    var buffer = await udpClient.ReceiveAsync();
+                    var str = Encoding.UTF8.GetString(buffer.Buffer);
+                    if (str.Substring(0, 6) == "volume")
+                    {
+                        int volume = int.Parse(str.Substring(6));
+                        SetMasterVolume(volume);
+                        continue;
+                    }
+                    switch (str)
+                    {
+                        case "shutdown":
+                            StartCMD("-s");
+                            break;
+                        case "restart":
+                            StartCMD("-r");
+                            break;
+                    }
+                    _ = udpClient.SendAsync(success, success.Length, buffer.RemoteEndPoint);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                }
+            }
+        }
+
+        public void SetMasterVolume(int volume)
+        {
+            Audio.Volume = volume;
+        }
+        void StartCMD(string order)
+        {
+            ProcessStartInfo psi = new ProcessStartInfo
+            {
+                FileName = "shutdown",
+                Arguments = $"{order} -t 0",
+                CreateNoWindow = true,
+                UseShellExecute = false
+            };
+            Process.Start(psi);
+        }
+        private void ContextMenu_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+            Application.Exit();
+        }
+        private void SetStartup(bool enable)
+        {
+            using (RegistryKey key = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true))
+            {
+                if (enable)
+                {
+                    // 设置开机自启
+                    key.SetValue(Application.ProductName, Application.ExecutablePath);
+                }
+                else
+                {
+                    // 取消开机自启
+                    key.DeleteValue(Application.ProductName, false);
+                }
+            }
+        }
+    }
+}
+[Guid("5CDF2C82-841E-4546-9722-0CF74078229A"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+interface IAudioEndpointVolume
+{
+    int _0(); int _1(); int _2(); int _3();
+    int SetMasterVolumeLevelScalar(float fLevel, Guid pguidEventContext);
+    int _5();
+    int GetMasterVolumeLevelScalar(out float pfLevel);
+    int _7(); int _8(); int _9(); int _10(); int _11(); int _12();
+}
+
+[Guid("D666063F-1587-4E43-81F1-B948E807363F"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+interface IMMDevice
+{
+    int Activate(ref System.Guid id, int clsCtx, int activationParams, out IAudioEndpointVolume aev);
+}
+
+[Guid("A95664D2-9614-4F35-A746-DE8DB63617E6"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+interface IMMDeviceEnumerator
+{
+    int _0();
+    int GetDefaultAudioEndpoint(int dataFlow, int role, out IMMDevice endpoint);
+}
+
+[ComImport, Guid("BCDE0395-E52F-467C-8E3D-C4579291692E")] class MMDeviceEnumeratorComObject { }
+public class Audio
+{
+    private static readonly IAudioEndpointVolume _MMVolume;
+
+    static Audio()
+    {
+        var enumerator = new MMDeviceEnumeratorComObject() as IMMDeviceEnumerator;
+        enumerator.GetDefaultAudioEndpoint(0, 1, out IMMDevice dev);
+        var aevGuid = typeof(IAudioEndpointVolume).GUID;
+        dev.Activate(ref aevGuid, 1, 0, out _MMVolume);
+    }
+
+    public static int Volume
+    {
+        get
+        {
+            _MMVolume.GetMasterVolumeLevelScalar(out float level);
+            return (int)(level * 100);
+        }
+        set
+        {
+            _MMVolume.SetMasterVolumeLevelScalar((float)value / 100, default);
+        }
+    }
+}
